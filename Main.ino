137d087f2d0388cc1c064ac25194af862c52cc8a
Christopher Nguyen <NguyenChrisV@gmail.com>
2023-11-24 01:27:40 -0600

    Added switch logic

--- Main.ino
+++ Main.ino
@@ -38,14 +38,19 @@ Adafruit_DotStarMatrix matrix = Adafruit_DotStarMatrix(
                                   DOTSTAR_BGR);
 
 
-void setup() {
-  Serial.begin(115200);
-  matrix.begin();
-  matrix.show(); // Initialize all pixels to 'off'
-}
+
 
 int x    = matrix.width();
+
 int pass = 0;
+const int buttonPin = 4;
+int flag = 0;
+
+// Variables will change:
+int buttonPushCounter = 0;  // counter for the number of button presses
+int buttonState = 0;        // current state of the button
+int lastButtonState = 0;    // previous state of the button
+
 #define EVER (;;);
 
 //cat face 
@@ -63,20 +68,107 @@ int sixArray [] = {9,10,11,12,13,14,17,14,22,25,30,33,38,41,46,49,50,51,52,53,54
 //8X8 square
 int eightArray [] = {0,1,2,3,4,5,6,7,8,15,16,23,24,31,32,39,40,47,48,55,56,57,58,59,60,61,62,63};
 
+void setup() {
+  Serial.begin(115200);
+  matrix.begin();
+  matrix.show(); // Initialize all pixels to 'off'
+
+  pinMode(buttonPin, INPUT);
+}
+
+
 void loop() {
 
   matrix.setBrightness(20 );
   matrix.fillScreen(0);
   matrix.setCursor(x, 0);
 
-  for (int pixel : catArray){
-    matrix.setPixelColor(pixel,255,0,0);
+  // read the pushbutton input pin:
+  buttonState = digitalRead(buttonPin);
+
+  // compare the buttonState to its previous state
+  if (buttonState != lastButtonState) {
+    // if the state has changed, increment the counter
+    if (buttonState == HIGH) {
+      // if the current state is HIGH then the button went from off to on:
+      if (buttonPushCounter < 1 ){
+        buttonPushCounter++;
+
+      }else {
+        buttonPushCounter = 0;
+
+      }
+      Serial.println("on");
+      Serial.print("number of button pushes: ");
+      Serial.println(buttonPushCounter);
+    } else {
+      // if the current state is LOW then the button went from on to off:
+      Serial.println("off");
+    }
+    // Delay a little bit to avoid bouncing
+    //delay(50);
+  }
+  // save the current state as the last state, for next time through the loop
+  lastButtonState = buttonState;
+
+
+  // turns on the LED every four button pushes by checking the modulo of the
+  // button push counter. the modulo function gives you the remainder of the
+  // division of two numbers:
+  if (buttonPushCounter == 0) {
+      for (int pixel : catArray){
+        matrix.setPixelColor(pixel,255,0,0);        
+      }
+
+      matrix.show();
+      delay(300);
+
+      for (int pixel : catArray){
+        matrix.setPixelColor(pixel,0,0,0);        
+      }
+
+      matrix.show();
+      delay(300);
+  } else {
+      for (int pixel : twoArray){
+        matrix.setPixelColor(pixel,255,0,0);
+      }
+      matrix.show();
+      delay(50);
+      
+      for (int pixel : fourArray){
+        matrix.setPixelColor(pixel,255,0,0);
+      }
+      matrix.show();
+      delay(20);
+      
+      for (int pixel : sixArray){
+        matrix.setPixelColor(pixel,255,0,0);
+      }
+      matrix.show();
+      delay(10);
+      
+      for (int pixel : twoArray){
+        matrix.setPixelColor(pixel,255,200,0);
+      }
+      
+      matrix.setBrightness(20);
+      matrix.show();
+      
+      delay(200);
+      
+      for (int pixel : twoArray){
+        matrix.setPixelColor(pixel,0,0,0);
+      }
+
+      matrix.show();
+
+
   }
 
 
-  matrix.show();
-
-  delay(100);
+  
+  //delay(100);
 }
 
 
